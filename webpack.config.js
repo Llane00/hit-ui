63c1d89d16b2cabdb1a9d3797e01b646ffd51bf5
Llane00 <bluetinestyle@126.com>
2021-08-07 12:25:01 +0800

    chroe: reset webapck.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
-module.export = {
-  //入口
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
   entry: {
-    index: './lib/index.tsx'
+    hit: path.join(__dirname, './lib/index.tsx')
+  },
+  output: {
+    path: path.join(__dirname, './dist/lib'),
+    library: 'HitUI',
+    libraryTarget: 'umd', //umd统一模块定义, CommonJs NodeJs规范module.export, amd浏览器规范define(functionx(){})
   },
   module: {
     rules: [
