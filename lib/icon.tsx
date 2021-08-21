efb52407947c02cca4d026f40acf7e09d264a32a
Llane00 <bluetinestyle@126.com>
2021-08-21 08:03:06 +0800

    fix: 添加 yarn test --watch 需要的配置文件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import './importIcons'
-import "./icon.scss"
+import './importIcons';
+import './icon.scss';
 import classes from './helpers/classes';
 
 interface IconProps extends React.SVGAttributes<SVGElement> {
