9e45d4fbe263d2fba6073b804aca716e45604109
Llane00 <bluetinestyle@126.com>
2021-08-21 08:12:17 +0800

    test: 给helper/classes方法增加单元测试

--- lib/helpers/__tests__/classes.unit.tsx
+++ lib/helpers/__tests__/classes.unit.tsx
@@ -10,4 +10,14 @@ describe('classes', () => {
     const result = classes('a', 'b');
     expect(result).toEqual('a b')
   })
+
+  it('接受undefined className', () => {
+    const result = classes('a', undefined);
+    expect(result).toEqual('a')
+  })
+
+  it('接受undefined和中文 className', () => {
+    const result = classes('a', undefined, '测试类');
+    expect(result).toEqual('a 测试类')
+  })
 })
