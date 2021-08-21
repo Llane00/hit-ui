50a4d765acbd32024721b17d3126e5e55d82f581
Llane00 <bluetinestyle@126.com>
2021-08-21 12:05:27 +0800

    chore: 设置version为0.0.1, 修改index.tsx导出格式

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,14 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Icon from './icon/icon';
-
-
-ReactDOM.render(<div>
-  <Icon name="up" 
-    className="test-icon"
-    onClick={()=>{console.log("onClick")}}
-    onMouseEnter={()=>{console.log("onMouseEnter")}}
-    onMouseMove={()=>{console.log("onMouseMove")}}
-    onMouseLeave={()=>{console.log("onMouseLeave")}}
-  />
-</div>, document.getElementById("root"));
+export {default as Icon} from './icon/icon';
