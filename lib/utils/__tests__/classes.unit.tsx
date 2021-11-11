569715b5f94375b27dae4ca4d50667faf616786f
Llane00 <bluetinestyle@126.com>
2021-11-11 19:13:57 +0800

    feat: 升级classPrefixMaker方法，增加单位测试

--- lib/utils/__tests__/classes.unit.tsx
+++ lib/utils/__tests__/classes.unit.tsx
@@ -1,23 +1,69 @@
-import classes from "../classes"
+import classPrefixMaker from '../classPrefixMaker'
 
-describe('classes', () => {
+describe('classPrefixMaker', () => {
+  const scopedClass = classPrefixMaker('hit-ui-test')
+
   it('接受1个 className', () => {
-    const result = classes('a');
-    expect(result).toEqual('a');
+    const result = scopedClass('a')
+    expect(result).toEqual('hit-ui-test-a')
   })
 
-  it('接受2个 className', () => {
-    const result = classes('a', 'b');
-    expect(result).toEqual('a b')
+  it('接受1个 className array', () => {
+    const result = scopedClass(['a', 'b'])
+    expect(result).toEqual('hit-ui-test-a hit-ui-test-b')
   })
 
-  it('接受undefined className', () => {
-    const result = classes('a', undefined);
-    expect(result).toEqual('a')
+  it('接受一个数组包含undefined和中文 className', () => {
+    const result = scopedClass(['a', '测试类'])
+    expect(result).toEqual('hit-ui-test-a hit-ui-test-测试类')
   })
 
-  it('接受undefined和中文 className', () => {
-    const result = classes('a', undefined, '测试类');
-    expect(result).toEqual('a 测试类')
+  it('接受一个className，且还有extra className', () => {
+    const result = scopedClass('a', {
+      extra: 'more',
+    })
+    expect(result).toEqual('hit-ui-test-a more')
+  })
+
+  it('接受一个空className数组，且还有extra className', () => {
+    const result = scopedClass([], {
+      extra: 'more',
+    })
+    expect(result).toEqual('more')
+  })
+
+  it('接受一个空className数组，且还有多个extra className', () => {
+    const result = scopedClass([], {
+      extra: 'more more2 more3',
+    })
+    expect(result).toEqual('more more2 more3')
+  })
+
+  it('接受一个数组包含undefined和中文 className，且还有extra className', () => {
+    const result = scopedClass(['a', '测试类'], {
+      extra: 'more',
+    })
+    expect(result).toEqual('hit-ui-test-a hit-ui-test-测试类 more')
+  })
+
+  it('综合测试', () => {
+    expect(scopedClass('')).toEqual('hit-ui-test')
+    expect(scopedClass('x')).toEqual('hit-ui-test-x')
+    expect(scopedClass(['a', { 'a-1': true }])).toEqual(
+      'hit-ui-test-a hit-ui-test-a-1'
+    )
+    expect(scopedClass([{ 'a-1': true }])).toEqual('hit-ui-test-a-1')
+    expect(scopedClass([{ 'a-1': true }, { 'a-2': true }])).toEqual(
+      'hit-ui-test-a-1 hit-ui-test-a-2'
+    )
+    expect(scopedClass([{ 'a-1': true }, { 'a-2': false }])).toEqual(
+      'hit-ui-test-a-1'
+    )
+    expect(
+      scopedClass([{ 'a-1': true }, { 'a-2': true }], { extra: 'more1 more2' })
+    ).toEqual('hit-ui-test-a-1 hit-ui-test-a-2 more1 more2')
+    expect(
+      scopedClass([{ 'a-1': true }, { 'a-2': false }], { extra: 'more1 more2' })
+    ).toEqual('hit-ui-test-a-1 more1 more2')
   })
 })
--- lib/utils/classPrefixMaker.tsx
+++ lib/utils/classPrefixMaker.tsx
@@ -1,7 +1,38 @@
-function classPrefixMaker(prefix:string) {
-  return function(classNameString:string) {
-    return [prefix, classNameString].filter(Boolean).join('-');
-  }
+/**
+ * classPrefixMaker 接收一个className的前缀 prefix
+ * @param prefix string 前缀
+ * @return function scpoedClass
+ *
+ * scopedClass 可以返回最终className
+ * @param classNameValue 需要带前缀的className
+ * @param IOptions 不需要带前缀的className
+ *
+ */
+interface IOptions {
+  extra: string | undefined
 }
 
-export default classPrefixMaker;
+type classTogglers = string | { [className: string]: boolean } | undefined
+
+function classPrefixMaker(prefix: string) {
+  return (classNameValue: string | Array<classTogglers>, options?: IOptions) =>
+    (classNameValue instanceof Array
+      ? classNameValue
+      : [{ [classNameValue]: classNameValue !== undefined }]
+    )
+      .map((item) => {
+        if (item && Object.values(item)[0] === true) {
+          return Object.keys(item)[0]
+        } else if (typeof item === 'string') {
+          return item
+        } else {
+          return undefined
+        }
+      })
+      .filter((v) => v !== undefined)
+      .map((item) => [prefix, item].filter(Boolean).join('-'))
+      .concat((options && options.extra) || [])
+      .join(' ')
+}
+
+export default classPrefixMaker
