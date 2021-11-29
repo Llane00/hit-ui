7103817db01de617036577fcdaf4118924d2b895
Llane00 <bluetinestyle@126.com>
2021-11-29 16:34:35 +0800

    chore: delete code

--- lib/dialog/index.tsx
+++
@@ -1,4 +0,0 @@
-import { Dialog, modal, alert, confirm } from './dialog'
-
-export default Dialog
-export { Dialog, modal, alert, confirm }
--- lib/layout/index.tsx
+++
@@ -1,8 +0,0 @@
-import Layout from './layout'
-import Header from './header'
-import Content from './content'
-import Footer from './footer'
-import Aside from './aside'
-
-export default Layout
-export { Layout, Header, Content, Footer, Aside }
